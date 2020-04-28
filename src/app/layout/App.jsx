125693ff99aa22dc0a69f1b4661336eb6eeb260f
Yuhua Guo <851707@ACDM.DS.SAIT.CA>
2020-04-27 23:04:17 -0600

    project-setup

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class App extends React.Component {
+  render(){
+  return (
+    <div>
+        <h1>Re-vents</h1>
+    </div>
+  );
+  }
+}
+
+export default App;
+
+
